33d351cc7ee8a0c9bdcee9fdd5e5ddafaf2dde6d
luisi <luis.alba.sanchez@efrei.net>
2022-05-28 21:24:50 +0200

    version plus jolie du site

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import streamlit as st
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
-# st.text_input("Your name", key="name")
-# st.session_state.name
-
+# ---- Onglet ----
 st.set_page_config(page_title='MyColon', page_icon='img/Colon_onglet.png')
 
-st.header("Generate ASCII images using GAN")
-st.subheader("Writer and Developer")
-st.write("Choose any image and get corresponding ASCII art:")
 
-uploaded_file = st.file_uploader("Select JPG file :",
-                                 ['jpg'],
-                                 help="Drag and drop the jpg file here")
+# ---- Title ----
+with st.container():
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        image_title = Image.open('img/Colon3.png')
+        st.image(image_title)
+    with col2:
+        st.header("Do you want to know your patient colon situation ?")
+    st.write("*This website allow you to know if your patient have a colon inflammation*")
 
-if uploaded_file is not None:
-    # src_image = load_image(uploaded_file)
-    image = Image.open(uploaded_file)
 
-    st.image(uploaded_file, caption='Input Image', use_column_width=True)
+# ---- image downloading ----
+with st.container():
+    st.write("##")
+    st.write("---")
+    st.write("##")
+    uploaded_file = st.file_uploader("Select JPG file :",
+                                     ['jpg'],
+                                     help="Drag and drop the jpg file here")
 
-st.markdown('Streamlit is **_really_ cool**.', unsafe_allow_html=True)
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(uploaded_file, caption='Image saved', use_column_width=True)
+
+
+# ---- A propos ----
+with st.container():
+    st.write("##")
+    st.write("---")
+    st.write("##")
+    with st.expander("A propos"):
+        st.write("""
+             bla bla bla.
+         """)
--- outils.py
+++ outils.py
@@ -1,5 +1,3 @@
-
-
 def toPrint_dictionnary(dict):
     for key, val in dict.items():
         print(key, val.shape)
